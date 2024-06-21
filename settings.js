de4326799848d354f548aeacbebba35087461484
MRFG <158528999+MRFGsn@users.noreply.github.com>
2024-06-21 18:26:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,20 +8,20 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94765476552' ;  // ADD YOUR NUMBER WITHOUT +
     
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=DUNWEDLQ#REnzG70LZN_-ZAnrzFWURo7i0Ig8xuODHGzXSH30CM4' ,
 travaSend: process.env.TRAVA_SEND || '25' ,    
-MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+MAX_SIZE: process.env.MAX_SIZE === undefined ? '2048': process.env.MAX_SIZE,/*add this in megabytes*/    
+OWNER_NAME: process.env.OWNER_NAME || 'Snyder' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech',
-MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://sachintharashan26:9hVStJbuE3uxwRtZ@cluster0.o0ec025.mongodb.net/' : process.env.MONGODB_URI,  
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgresql://mrfg_database_user:MYoTnmpsoDcxBIX0U8V1IJ6yXRzEEnh7@dpg-comundgcmk4c739roc3g-a.singapore-postgres.render.com/mrfg_database',
+MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://Snyder:Snyder@cluster0.vqyuk9h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' : process.env.MONGODB_URI,  
 FOOTER: process.env.FOOTER || '‌👨‍💻 ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 👨‍💻',    
-PREFIX: process.env.PREFIX || '.' ,
+PREFIX: process.env.PREFIX || ',' ,
 LANG:  process.env.LANG || 'SI' ,    
 ANTI_BAD: process.env.ANTI_BAD || false  ,
 AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
