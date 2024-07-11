f739d0d2d71755bb5963dc92246283a430793033
BLACK DEVIL <158528999+MRFGsn@users.noreply.github.com>
2024-07-11 19:48:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,15 +12,15 @@ global.owner = process.env.OWNER_NUMBER  || '94765476552' ;  // ADD YOUR NUMBER 
     
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=eY1GmbpA#rCg7BX8Qtjm7biyNLVS_XI3OI6hmGNX84n8rny55da0' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=6M1zFR5Y#moCkPfF40bj8EPKsznuWeNq1CmX9Po79s1rW3OB3tIs' ,
 travaSend: process.env.TRAVA_SEND || '25' ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '2048': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'Snyder' ,
-COMMAND_TYPE: process.env.COMMAND_TYPE || 'text' ,
+COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgresql://mrfg_database_user:MYoTnmpsoDcxBIX0U8V1IJ6yXRzEEnh7@dpg-comundgcmk4c739roc3g-a.singapore-postgres.render.com/mrfg_database',
-MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://Snyder:Snyder@cluster0.vqyuk9h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' : process.env.MONGODB_URI,  
-FOOTER: process.env.FOOTER || '‌👨‍💻 ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 👨‍💻',    
+MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://Snyder:Snyder@liya.cluiny3.mongodb.net/?retryWrites=true&w=majority&appName=Liya' : process.env.MONGODB_URI,  
+FOOTER: process.env.FOOTER || 'mongodb+srv://Snyder:Snyder@liya.cluiny3.mongodb.net/?retryWrites=true&w=majority&appName=Liya',    
 PREFIX: process.env.PREFIX || ',' ,
 LANG:  process.env.LANG || 'SI' ,    
 ANTI_BAD: process.env.ANTI_BAD || false  ,
